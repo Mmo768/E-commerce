79711b9856a4a4553bab9bd4cf8a508126ab9ea5
Mmo768 <Mohamedmansoura10@gmail.com>
2023-04-09 20:45:20 +0200

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { CartService } from 'src/app/services/cart.service';
+import { Subscription } from 'rxjs';
+
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  Error!:string;
+  isLoading:boolean = false;
+  sub:Subscription = new Subscription();
+
+
+  constructor(private _AuthService:AuthService , private _Router:Router,private _CartService:CartService){}
+
+
+
+
+  loginForm:FormGroup = new FormGroup({
+    email: new FormControl(null,[Validators.required , Validators.email]),
+    password: new FormControl(null,[Validators.required , Validators.pattern(/^[A-Z]\w{2,}\w{3,}/)]),
+});
+
+
+
+onSubmit(loginForm:FormGroup){
+  this.isLoading = true;
+  this.sub.add(  this._AuthService.signIn(loginForm.value).subscribe({
+    next:(reponse)=>{
+      if(reponse.message == "success"){
+        localStorage.setItem('userToken' , reponse.token);
+        this._AuthService.getLocalToken();
+        this._CartService.getCartOwner();
+        this._Router.navigate(['/Home']);
+        this.isLoading = false;
+      }
+      
+    },
+    error:(err)=>{
+      this.Error = err.error.message;
+      this.isLoading = false;
+      
+    }
+  }));
+
+}
+
+
+    //========= start on destroy ==========
+    ngOnDestroy(){
+      this.sub.unsubscribe();
+  
+    }
+
+
+
+
+
+
+
+}
---
+++ src/app/components/order/order.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { LoadService } from 'src/app/services/load.service';
+import { Subscription } from 'rxjs';
+
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent {
+  dataOrder:any;
+  sub:Subscription = new Subscription();
+
+  constructor(private _CartService:CartService,private _LoadService:LoadService){}
+
+  ngOnInit(){
+    this._LoadService.isTrue();
+    this.sub.add(    this._CartService.cartOwner.subscribe((x)=>{
+      if(x!=null){
+        
+        this._CartService.getUserOrder(x).subscribe({
+          next:(response)=>{
+            this.dataOrder = response;
+            this._LoadService.isFalse();
+          },
+          error:(err)=>{
+            console.log('order > onInit' , err);
+            
+          }})
+      }}));
+
+
+
+  }
+
+
+    //========= start on destroy ==========
+    ngOnDestroy(){
+      this.sub.unsubscribe();
+  
+    }
+
+}
